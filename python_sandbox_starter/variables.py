cbd91ad23482c53a1eaaa638aed1a875e6114423
mohamedlafiteh <m_lafiteh@yahoo.com>
2020-03-08 22:45:33 +0000

    First commit

--- python_sandbox_starter/variables.py
+++ python_sandbox_starter/variables.py
@@ -13,3 +13,10 @@ VARIABLE RULES:
   - Must start with a letter or an underscore
   - Can have numbers but can not start with one
 """
+#x = 1 #int
+ #y =2.5 
+#name = 'John'
+#is_cool = True
+
+x,y,name,is_cool = (1,2.5,'John',True)
+print(x)
